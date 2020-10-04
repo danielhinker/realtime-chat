b1d1cdb03a2f99f1fe9bae46d91068f43a613dfb
danielhinker <danhinker@gmail.com>
2020-10-03 21:02:03 -0400

    Add mongoose-encryption

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const mongoose = require("mongoose");
 bodyParser = require('body-parser');
+const encrypt = require("mongoose-encryption");
 
 // support parsing of application/json type post data
 app.use(bodyParser.json());
@@ -15,13 +17,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = ({
+const userSchema = new mongoose.Schema({
   username: String,
   password: String
 })
 
-const User = new mongoose.model("User", userSchema)
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']})
 
+const User = new mongoose.model("User", userSchema)
+ 
 app.get("/register", function(req, res){
   res.sendFile(__dirname + '/register.html');
 });
